dafe4c447640e0c35bc6ab22e1261d9d40326e33
Manuel Hinojosa <mangud.hinojosa@gmail.com>
2023-03-20 16:26:32 -0400

    9: Created logging.ts in library dir, Used chalk dec to costume logs instead of using console.log function. Removed all console log statements, imported logging class and used logging to substitute all console.log statements

--- src/config/connectdb.ts
+++ src/config/connectdb.ts
@@ -1,12 +1,13 @@
 import mongoose from 'mongoose';
 import { config } from './config';
+import Logging from '../library/logging';
 
 mongoose
     .connect(config.mongo.url)
     .then(() => {
-        console.log('moongo connection open');
+        Logging.info("connected to MongoDB's Atlas");
     })
-    .catch((err: any) => {
-        console.log('mongo connection error');
-        console.log(err);
+    .catch((error: any) => {
+        Logging.error("Unable to connect to MongoDB's Atlas");
+        Logging.error(error);
     });
