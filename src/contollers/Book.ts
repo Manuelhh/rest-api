62ea8997d3359902115bbcd02251dba6604eb090
Manuel Hinojosa <mangud.hinojosa@gmail.com>
2023-03-22 12:22:32 -0400

    16: Created book ts file in models dir. Created and exported book model in model.ts, mported mongoose, Author model and express req into it, Created createAuthor function, Created find one author function, Created find all authors function, Created update one author by id function, Created delete one author by id function, Exported all functions

---
+++ src/contollers/Book.ts
@@ -0,0 +1,75 @@
+// from express for ts func
+import { Request, Response, NextFunction } from 'express';
+// dependencies
+import mongoose from 'mongoose';
+//Author model
+import Book from '../models/Book';
+
+// create a book:
+const createBook = (req: Request, res: Response, next: NextFunction) => {
+    const { title, author } = req.body;
+
+    const book = new Book({
+        _id: new mongoose.Types.ObjectId(),
+        title,
+        author
+    });
+
+    return book
+        .save()
+        .then((book) => res.status(201).json({ book }))
+        .catch((error) => res.status(500).json(error));
+};
+
+// get 1 book by id:
+const readBook = (req: Request, res: Response, next: NextFunction) => {
+    const bookId = req.params.bookId;
+
+    return Book.findById(bookId)
+        .then((book) => (book ? res.status(200).json({ book }) : res.status(404).json({ message: 'not found' })))
+        .catch((error) => res.status(500).json({ error }));
+};
+
+// get all books:
+const readAllBooks = (req: Request, res: Response, next: NextFunction) => {
+    return Book.find()
+        .then((books) => res.status(200).json({ books }))
+        .catch((error) => res.status(500).json({ error }));
+};
+
+// update one book
+const updateBook = (req: Request, res: Response, next: NextFunction) => {
+    const bookId = req.params.bookId;
+
+    return Book.findById(bookId)
+        .then((book) => {
+            if (book) {
+                book.set(req.body);
+
+                return book
+                    .save()
+                    .then((book) => res.status(201).json({ book }))
+                    .catch((error) => res.status(500).json({ error }));
+            } else {
+                res.status(404).json({ message: 'not found' });
+            }
+        })
+        .catch((error) => res.status(500).json({ error }));
+};
+
+// delete one book by ID
+const deleteBook = (req: Request, res: Response, next: NextFunction) => {
+    const bookId = req.params.bookId;
+
+    return Book.findByIdAndDelete(bookId)
+        .then((book) => (book ? res.status(201).json({ message: 'deleted' }) : res.status(404).json({ message: 'nof found' })))
+        .catch((error) => res.status(500).json({ error }));
+};
+
+export default {
+    createBook,
+    readBook,
+    readAllBooks,
+    updateBook,
+    deleteBook
+};
